c177586273e067648f4233e9fef0e1bd59f0710f
Joe <jakatsa5@gmail.com>
2024-03-16 11:52:52 +0300

    product cards design

--- ecomm/src/components/Product.jsx
+++ ecomm/src/components/Product.jsx
@@ -1,9 +1,62 @@
 import React from "react";
-
+//import link from react router in app.jsx
+import { Link } from "react-router-dom";
+//import icons
+import { BsPlus, BsEyeFill } from "react-icons/bs";
 export const Product = ({ product }) => {
+  console.log(product);
+  //destructure product
+  const { id, image, category, title, price } = product;
   return (
-    <div className="w-full h-{300px} bg-pink-200 " key={product.title}>
-      {product.title}
+    <div>
+      <div
+        className="border border-[#e4e4e4] h-[300px] mb-4
+      relative overflow-hidden group transition"
+      >
+        <div className="w-full h-full flex justify-center items-center">
+          {/*image*/}
+          <div className="w-[200px] mx-auto flex justify-center items-center">
+            <img
+              className="max-h-[160px] group-hover:scale-110 transition
+              duration-300"
+              src={image}
+              alt="product image"
+            />
+          </div>
+        </div>
+        {/**button */}
+        <div
+          className="absolute top-6 right-11 group-hover:right-5
+          p-2 flex flex-col items-center justify-center gap-y-2
+           opacity-0 group-hover:opacity-100 transition-all
+           duration-300"
+        >
+          <button>
+            <div
+              className="flex justify-center items-center
+              text-white w-12 h-12 bg-red-500"
+            >
+              <BsPlus className="text-3xl" />
+            </div>
+          </button>
+          <Link
+            to={`/product/${id}`}
+            className="w-12 h-12 bg-white flex justify-center
+            items-center text-primary drop-shadow-xl "
+          >
+            <BsEyeFill />
+          </Link>
+        </div>
+      </div>
+      {/** categories*/}
+
+      <div>
+        <div className="text-sm capitalize text-gray-500 mb-1">{category}</div>
+        <Link to={`/product/${id}`}>
+          <h2 className="font-semibold mb-1">{title}</h2>
+        </Link>
+        <div className="font-semibold">{price}</div>
+      </div>
     </div>
   );
 };

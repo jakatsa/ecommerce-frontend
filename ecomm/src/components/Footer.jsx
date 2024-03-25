739e318e74a0551ccedc0eb723be0555fac66050
jakatsa <118554412+jakatsa@users.noreply.github.com>
2024-03-25 06:09:46 +0300

    footer updates

--- ecomm/src/components/Footer.jsx
+++ ecomm/src/components/Footer.jsx
@@ -1,3 +1,6 @@
+{
+  /**footer */
+}
 import React from "react";
 
 export const Footer = () => {
@@ -5,7 +8,8 @@ export const Footer = () => {
     <footer className="bg-primary py-12">
       <div className="container mx-auto">
         <p className="text-white text-center">
-          Copyright &copy; E-commerce Shop 2024.All Rights By Joe,Leyian,Hansel and Ian.
+          Copyright &copy; E-commerce Shop 2024.All Rights By Joe,Leyian,Hansel
+          and Ian.
         </p>
       </div>
     </footer>

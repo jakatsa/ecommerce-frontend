21169a41d2820d69ce4b7400c79dfa229370bde8
Joe <jakatsa5@gmail.com>
2024-03-25 08:21:01 +0300

    vite config updates

--- ecomm/vite.config.js
+++ ecomm/vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ecommerce-frontend"
-})
+  base: "/ecommerce-frontend", // Set the base URL if needed
+  build: {
+    // Set the entry point to your main JavaScript file
+    rollupOptions: {
+      input: {
+        main: "./main.jsx", // Adjust the path as needed
+      },
+    },
+  },
+});
